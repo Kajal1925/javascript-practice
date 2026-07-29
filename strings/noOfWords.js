c9ec59eec60342ee74bf525dd628e6ebb3464716
Kajal1925 <kajalkumari2622021@gmail.com>
2026-07-29 18:43:35 +0530

    Add files via upload

---
+++ strings/noOfWords.js
@@ -0,0 +1,3 @@
+let sentence = "Learning this language";
+let arr = sentence.split(" ");// this method will convert the string to the array of words;
+console.log("The number of words in the sentence = ",arr.length);
